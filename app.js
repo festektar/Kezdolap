58027ef11458f12e1909835d5766480ee54648d3
festektar <133231650+festektar@users.noreply.github.com>
2023-05-11 19:11:26 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -15,3 +15,47 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElement = document.querySelectorAll('.hidden');
 hiddenElement.forEach((el) => observer.observe(el));
+
+
+
+const observer_Y = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+
+
+
+const hidden_YElement = document.querySelectorAll('.hidden-Y');
+hidden_YElement.forEach((el) => observer_Y.observe(el));
+
+
+
+
+
+
+
+
+
+const observer_YX = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+
+
+
+const hidden_YXElement = document.querySelectorAll('.hidden-YX');
+hidden_YXElement.forEach((el) => observer_YX.observe(el));
